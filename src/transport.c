c6e18a6e14e8ab4ca3f404454a7d23f490e50287
Jaime Forero <j.e.forero.romero@gmail.com>
2013-03-13 15:06:01 -0500

    new transport.c files

---
+++ src/transport.c
@@ -0,0 +1,26 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <math.h>
+#include "struct.h"
+#include "scatter.h"
+
+
+void TransportPhotons(float *x, float *p, float *k, int n_photons){
+  int pack_size, last_pack_size;  
+  int n_packs;
+  int i;
+
+  pack_size = 512;
+  
+  n_packs = n_photons/pack_size;
+  last_pack_size = n_photons%pack_size;
+  
+  fprintf(stdout, "Photons to transport %d\n", n_photons);
+  fprintf(stdout, "%d packs of size %d\n", n_packs, pack_size);
+  fprintf(stdout, "one last pack of size %d\n", last_pack_size);
+  
+  for(i=0;i<n_packs;i++){
+    scatter_bunch(x, p, i*pack_size, (i+1)*pack_size);
+  }  
+  scatter_bunch(x, p, i*pack_size, i*pack_size + last_pack_size);  
+}
---
+++ src/transport.h
@@ -0,0 +1,4 @@
+#ifndef SCATTER_H
+#define SCATTER_H
+void TransportPhotons(float *x, float *p, float *k, int n_photons);
+#endif
